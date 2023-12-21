470ffb3d892da9864218c4f5d2ba2f607cd805a8
Your Name <elisius@live.dk>
2023-12-21 14:31:00 +0100

    efter vejledning 21 dec

--- BCI_fit.jl
+++ BCI_fit.jl
@@ -46,18 +46,18 @@ without using a HGF substruct
 # I dont know if i need to convert the sigmas to variances
 
 original_action_model = function(agent::Agent, input, constant_cue = "A", decision = "model_averaging")
-""" 
-constant_cue can be either "A" or "V" and is used to specify whether the cue is auditory or visual
-decision can be either "model_averaging" or "model_selection" and is used to specify whether the decision rule is model averaging or model selection
-"""
+    """ 
+    constant_cue can be either "A" or "V" and is used to specify whether the cue is auditory or visual
+    decision can be either "model_averaging" or "model_selection" and is used to specify whether the decision rule is model averaging or model selection
+    """
 
     if length(input) == 3
-        auditory_stimulus = input[1]
-        visual_stimulus = input[2]
+        auditory_stimulus = input[1] * randn(1) # added random noise
+        visual_stimulus = input[2] * randn(1) # added random noise
         cue = input[3]
     else
-        auditory_stimulus = input[1]
-        visual_stimulus = input[2]
+        auditory_stimulus = input[1] * randn(1) # added random noise
+        visual_stimulus = input[2] * randn(1) # added random noise 
         cue = constant_cue
     end
 
@@ -67,6 +67,7 @@ decision can be either "model_averaging" or "model_selection" and is used to spe
     sigP = agent.parameters["sigP"]
     sigA = agent.parameters["sigA"]
     sigV = agent.parameters["sigV"]
+    action_noise = agent.parameters["action_noise"]
 
     # variances of A and V and prior
     varP = sigP^2
@@ -78,6 +79,7 @@ decision can be either "model_averaging" or "model_selection" and is used to spe
     varV_hat = 1 / ( 1 / varV + 1 / varP )
     varA_hat = 1 / ( 1 / varA + 1 / varP ) =#
     # I drop this as it is just confusing code practice when realting it to the used equations in papers
+    # It is instead part of the sAV_hat_if_common equation
 
     # variances used in computing probasbility of common or independent causes
     var_common = varV * varA + varV * varP + varA * varP
@@ -96,26 +98,15 @@ decision can be either "model_averaging" or "model_selection" and is used to spe
     
     # udregn prob of common or independent
     ## this is a weighted distance metric
-    #= quad_common = ( visual_stimulus - auditory_stimulus )^2 * varP + ( visual_stimulus - muP )^2 * varA + ( auditory_stimulus - muP )^2 * varV
+    quad_common = ( visual_stimulus - auditory_stimulus )^2 * varP + ( visual_stimulus - muP )^2 * varA + ( auditory_stimulus - muP )^2 * varV
     quadV_independent = ( visual_stimulus - muP )^2
-    quadA_independent = ( auditory_stimulus - muP )^2 =#
+    quadA_independent = ( auditory_stimulus - muP )^2
 
     # likelihood of observations (xV, xA) given C (1=common or 2=independent)
     ## this is the PDF
-    #= likelihood_common = exp(-quad_common/(2*var_common)) / (2*pi*sqrt(var_common))
+    likelihood_common = exp(-quad_common/(2*var_common)) / (2*pi*sqrt(var_common))
     likelihoodV_independent = exp(-quadV_independent/(2*varV_independent)) / sqrt(2*pi*varV_independent)
     likelihoodA_independent = exp(-quadA_independent/(2*varA_independent)) / sqrt(2*pi*varA_independent)
-    likelihood_independent = likelihoodV_independent * likelihoodA_independent
- =#
-
-    # this is multivariate normal pdf
-#= 
-    likelihood_common = pdf( MvNormal(zeros(3), [varV, varA, varP]), [visual_stimulus, auditory_stimulus, muP] )
- =#
-
-    likelihood_common = pdf( Normal(0, var_common), sAV_hat_if_common )
-    likelihoodV_independent = pdf( Normal(0, varV_independent), S_V_if_independent )
-    likelihoodA_independent = pdf( Normal(0, varA_independent), S_A_if_independent )
     likelihood_independent = likelihoodV_independent * likelihoodA_independent
 
     # posterior probability of state C (cause 1 or 2) given observations (xV, xA)
@@ -132,17 +123,27 @@ decision can be either "model_averaging" or "model_selection" and is used to spe
         sA_hat = (posterior_C > 0.5) * sA_hat_if_common + (posterior_C <= 0.5) * S_A_if_independent
     end
 
-    
-    return posterior_C, sV_hat, sA_hat
+    if cue == "A"
+        action = Normal(sA_hat, action_noise)
+    elseif cue == "V"
+        action = Normal(sV_hat, action_noise)
+    end
+
+    return action
 end
+
+# If i wanted to save the posterior_C 
+#agent.states["C"] = posterior_C
+#push!(agent.history["C"], posterior_C)
 
 original_params = Dict(
     #PArametrs: 
     "p_common" => 1 ,# p_common - prior for Common or not
-    "muP" => 1,# muP - centrality bias
+    "muP" => 0,# muP - centrality bias
     "sigP" => 1,# sigP - position variability from 0
     "sigA" => 1,# sigA - auditory noise
     "sigV" => 1,# sigV - visual noise
+    "action_noise" => 1
 )
 
 #States:
@@ -150,11 +151,18 @@ original_params = Dict(
 # S_AV - the shared position
 # S_A - the auditory position
 # S_V - the visual position
+original_states = [
+    Dict("name" => "C"),
+    Dict("name" => "sAV"),
+    Dict("name" => "sA"),
+    Dict("name" => "sV"),
+]
+
 original_states = Dict(
-    "name" => "C",
-    "name" => "S_AV",
-    "name" => "S_A",
-    "name" => "S_V"
+    "C" => 1,
+    "sAV" => 0,
+    "sA" => 0,
+    "sV" => 0,
 )
 
 priors = Dict(
@@ -163,7 +171,11 @@ priors = Dict(
     "sigA" => Normal(0, 1),
     "sigV" => Normal(0, 1),
     "muP" => Normal(0, 1),
+    "action_noise" => LogNormal(0,0.2),
 )
+# prøv at fit uden muP
+# og smallere priors
+
 
 agent = init_agent(
     original_action_model,
@@ -186,16 +198,106 @@ inputs = Array( [rand(values, 2) for _ in 1:num_samples] )
 # To view the first few samples
 println(inputs[1:10])
 
+get_parameters(agent)
+
+give_inputs!(agent, inputs)
+get_history(agent)
+plot_trajectory(agent, "C") 
+plot_trajectory(agent, "action")
+
+inputs
+
+# REAL DATA
+
+dataset = CSV.read("park_and_kayser2023.csv", DataFrame)
+
+show(dataset)
+
+dataset[!, "input"] = JSON.parse.(dataset[!, "input"])
+
+typeof(dataset[!,"input"])
+
+typeof(dataset[!,"action"])
+
+inputs = dataset[:,1]
+
+# I try with experiment 1 only
+df_exp1 = dataset[dataset[!, "experiment"] .== "experiment 1", :]
+inputs_exp1 = df_exp1[:,1]
+actions_exp1 = df_exp1[:,2]
+
+reset!(agent)
+give_inputs!(agent, inputs_exp1)
+
+plot_trajectory(agent, "action")
+
+agent.history
+
 # Fitting the model to the simulated data
-fit_model(
+chains = fit_model(
     agent,
     priors,
-    inputs,
-    actions,
-    n_iterations = 2000
+    inputs_exp1,
+    actions_exp1,
+    n_iterations = 2000,
+)
+
+serialize("bci_fit_19-12-23.jls", chains)
+
+plot(chains)
+
+
+
+auditory_input, visual_input
+
+input_cols = [:auditory_input, :visual_input]
+action_cols = [:action]
+independent_group_cols = [:experiment, :participant]
+
+chains = fit_model(
+    agent,
+    priors,
+    dataset;
+    input_cols = input_cols,
+    action_cols = action_cols,
+    independent_group_cols = independent_group_cols,
+    n_iterations = 1000,
+    n_cores = 4,
+    n_chains = 2,
 )
 
 
+
+
+
+
+input_cols = [:auditory_input, :visual_input]
+action_cols = [:action]
+independent_group_cols = [:experiment]
+multilevel_cols = [:participant]
+
+priors = Dict(
+    ("xA", "input_noise") => Multilevel(
+        :participant,
+        LogNormal,
+        ["xA_noise_group_mean", "xA_noise_group_sd"]
+    ),
+    "xA_noise_group_mean" => Normal(0, 1),
+    "xA_noise_group_sd" => LogNormal(0, 1),
+)
+
+chains = fit_model(
+    agent,
+    priors,
+    dataset;
+    input_cols = input_cols,
+    action_cols = action_cols,
+    independent_group_cols = independent_group_cols,
+    multilevel_cols = multilevel_cols,
+    n_iterations = 1000,
+    n_cores = 4,
+    n_chains = 2,
+)
 
 
 
---
+++ merging_hgf copy.jl
@@ -0,0 +1,280 @@
+"""
+Merging HGF 
+
+"""
+
+
+# Packages
+using DataFrames, CSV, TimeSeries, Serialization, JSON
+using Distributions
+using Plots, StatsPlots
+using ActionModels, HierarchicalGaussianFiltering
+using Turing
+using CategoricalArrays
+using ForwardDiff
+
+"This is for the merging HGF"
+forced_fusion
+# FF_sAV
+# FF_xA
+# FF_xV
+
+Independet
+# Ind_sA
+# Ind_sV
+# Ind_xA
+# Ind_xV
+
+model_combination
+# C - cause
+
+
+
+edges, nodes
+
+init_hgf
+
+#List of input nodes to create
+input_nodes = [
+    Dict("name" => "xA"),
+    Dict("name" => "xV"),
+]
+
+
+#List of state nodes to create
+state_nodes = [
+    Dict("name" => "FF_sAV"),
+    Dict("name" => "Ind_sA"),
+    Dict("name" => "Ind_sV"),
+    Dict("name" => "xC"),
+    Dict("name" => "C"),
+]
+
+edges = [
+    # Forced fusion
+    Dict("child" => "xA", "value_parents" => ("FF_sAV")),
+    Dict("child" => "xV", "value_parents" => ("FF_sAV")),
+    # Independent
+    Dict("child" => "xA", "value_parents" => ("Ind_sA")),
+    Dict("child" => "xV", "value_parents" => ("Ind_sV")),
+    # Cause
+    Dict("child" => "xC", "value_parents" => ("C")),
+]
+
+
+#Initialize the HGF
+hgf = init_hgf(
+    input_nodes = input_nodes,
+    state_nodes = state_nodes,
+    edges = edges,
+)
+
+print( get_parameters(hgf) )
+
+print( get_states(hgf) )
+
+get_surprise(hgf)
+
+function merging_hgf(agent::Agent, input, constant_cue="A", decision="model_averaging_surprise")
+
+    if length(input) == 3
+        auditory_stimulus = input[1]
+        visual_stimulus = input[2]
+        cue = input[3]
+    else
+        auditory_stimulus = input[1]
+        visual_stimulus = input[2]
+        cue = constant_cue
+    end
+
+    action_noise = agent.parameters["action_noise"]
+    prior_common_cause = agent.parameters["p_common"]
+
+    hgf = agent.substruct
+
+    # update HGF with inputs
+    update_hgf!(hgf, [auditory_stimulus, visual_stimulus])
+
+    # Forced Fusion (Common Cause)
+    FF_inferred_location = get_states(hgf, ("FF_sAV", "posterior_mean"))
+
+    FF_action_distribution = Normal(FF_inferred_location, action_noise)
+
+    # Segregation (Independent Causes)
+    if cue == "A"
+        Ind_inferred_location = get_states(hgf, ("Ind_sA", "posterior_mean"))
+    elseif cue == "V"
+        Ind_inferred_location = get_states(hgf, ("Ind_sV", "posterior_mean"))
+    end
+
+    Ind_action_distribution = Normal(Ind_inferred_location, action_noise)
+
+    # CAUSE estimation
+    ## calculating likelihoods
+
+    #FF_likelihood_auditory = pdf(MvNormal(FF_inferred_location, FF_inferred_location]))
+ #= 
+    Ind_likelihood_auditory = pdf(Ind_action_distribution, auditory_stimulus)
+    Ind_likelihood_visual = pdf(Ind_action_distribution, visual_stimulus)
+    likelihood_independent = Ind_likelihood_auditory * Ind_likelihood_visual
+ =#
+
+    ## Model Averaging using surprise
+    surprise_common_cause = get_surprise(hgf, ("xA"))
+    
+    surpriseA_independent = get_surprise(hgf, ("xA"))
+    surpriseV_independent = get_surprise(hgf, ("xV"))
+    surprise_independent = surpriseA_independent + surpriseV_independent
+
+    
+    posterior_common = exp(surprise_common_cause) * prior_common_cause
+    posterior_independent = exp(surprise_independent) * (1 - prior_common_cause)
+    posterior_C = posterior_common / ( posterior_common + posterior_independent )
+    
+
+    # DECISION
+    ## Model averaging w. surprise
+    if decision == "model_averaging_surprise"
+        sV_hat = posterior_C * FF_inferred_location + (1 - posterior_C) * Ind_inferred_location
+        sA_hat = posterior_C * FF_inferred_location + (1 - posterior_C) * Ind_inferred_location
+    end
+    ## 
+
+    if cue == "A"
+        action = Normal(sA_hat, action_noise)
+    elseif cue == "V"
+        action = Normal(sV_hat, action_noise)
+    end
+
+    return action
+end
+
+
+
+print(get_states(hgf))
+
+agent_parameters = Dict(
+    "action_noise" => 1,
+    "p_common" => 0.5
+)
+
+agent = init_agent(
+    merging_hgf,
+    parameters = agent_parameters,
+    substruct = hgf,
+)
+
+priors = Dict(
+    ("xV", "input_noise") => Normal(-2, 1),
+    ("xA", "input_noise") => Normal(-2, 1),
+    "action_noise" => LogNormal(0,0.2),
+    "p_common" => Normal(0,1),
+)
+
+# simulate some data
+values = [-22, -11, 0, 11, 22]
+## Number of samples
+num_samples = 1000
+## Generate samples
+actions = Array(rand(values, num_samples))
+## To view the first few samples
+println(actions[1:10])
+# Generate samples of vectors consisting of two values
+inputs = Array( [rand(values, 2) for _ in 1:num_samples] )
+# To view the first few samples
+println(inputs[1:10])
+
+# simulate some data
+
+reset!(agent)
+give_inputs!(agent, inputs)
+
+plot_trajectory(agent, "xA")
+plot_trajectory!(agent, "xV")
+plot_trajectory!(agent, "FF_sAV")
+plot_trajectory!(agent, "Ind_sA")
+plot_trajectory!(agent, "Ind_sV")
+plot_trajectory!(agent, "C")
+plot_trajectory!(agent, "action")
+
+action_history = get_history(agent, "action")
+
+action_history
+
+# TRYING WITH Dataset
+
+# load in dataset
+dataset = CSV.read("park_and_kayser2023.csv", DataFrame)
+
+show(dataset)
+
+#doin some cleaning to get the numerical values
+dataset[!, "input"] = JSON.parse.(dataset[!, "input"])
+
+typeof(dataset[!,"input"])
+
+typeof(dataset[!,"action"])
+
+show(dataset)
+
+inputs = dataset[:,1]
+
+inputs
+
+reset!(agent)
+give_inputs!(agent, inputs)
+
+plot_trajectory(agent, "xA")
+plot_trajectory!(agent, "xV")
+plot_trajectory!(agent, "FF_sAV")
+plot_trajectory!(agent, "Ind_sA")
+plot_trajectory!(agent, "Ind_sV")
+plot_trajectory!(agent, "C")
+plot_trajectory!(agent, "action")
+
+action_history = get_history(agent, "action")
+
+action_history
+
+# Fitting model with all experiments multilevel
+
+# I try with experiment 1 only
+df_exp1 = dataset[dataset[!, "experiment"] .== "experiment 1", :]
+inputs_exp1 = df_exp1[:,1]
+actions_exp1 = df_exp1[:,2]
+
+actions_exp1
+
+reset!(agent)
+give_inputs!(agent, inputs_exp1)
+
+plot_trajectory(agent, "xA")
+plot_trajectory!(agent, "xV")
+plot_trajectory!(agent, "action")
+
+action_history = get_history(agent, "action")
+action_history_exp1 = action_history[2:length(action_history)]
+action_history_exp1 = ForwardDiff.value.(action_history_exp1)
+
+
+action_history_exp1
+print(action_history_exp1)
+
+# save csv of inputs_exp1 and actions_history_exp1
+df_exp1_simulated_actions = DataFrame(
+    input = inputs_exp1,
+    action = action_history_exp1,
+)
+
+df_exp1_simulated_actions
+
+CSV.write("df_exp1_simulated_actions.csv", df_exp1_simulated_actions)
+
+chains = fit_model(
+    agent,
+    priors,
+    inputs_exp1,
+    actions_exp1,
+    n_iterations = 2000,
+)
+
